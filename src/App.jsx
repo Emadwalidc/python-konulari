1b48866d829748887facd5ad738240a95272f956
emad_walid <emad.walidc@gmail.com>
2024-12-31 02:01:12 +0300

    final2

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Header from './components/Header/Header.jsx';
 import CoreConcepts from './components/CoreConcepts.jsx';
 import Examples from './components/Examples.jsx';
 import VideoGallery from './components/VideoGallery.jsx';
-import SignUp from './components/Signup.jsx';
+import SignUp from './components/SignUp.jsx';
 import ScrollToTop from './components/ScrollToTop.jsx';
 import Footer from './components/Footer.jsx';
 import AboutPython from './components/AboutPython.jsx';
